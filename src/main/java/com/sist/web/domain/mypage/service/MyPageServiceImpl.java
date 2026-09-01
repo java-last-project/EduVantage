16e5061f337caea3832685d9505d13b110ac21b8
smsong <93063388+skc4234@users.noreply.github.com>
2026-09-01 17:49:31 +0900

    Feature/mypage (#16)
    * skc4234) feat: 마이페이지 레이아웃 완성 및 DB 연결

---
+++ src/main/java/com/sist/web/domain/mypage/service/MyPageServiceImpl.java
@@ -0,0 +1,37 @@
+package com.sist.web.domain.mypage.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sist.web.domain.mypage.vo.CourseEnrollmentVO;
+import com.sist.web.domain.mypage.vo.MyMemberVO;
+import com.sist.web.domain.mypage.mapper.*;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MyPageServiceImpl implements MyPageService {
+	private final MyPageMapper mMapper;
+	@Override
+	public List<CourseEnrollmentVO> mypageCourseListData(int member_id) {
+		// TODO Auto-generated method stub
+		return mMapper.mypageCourseListData(member_id);
+	}
+	@Override
+	public int enrolledCount(int member_id) {
+		// TODO Auto-generated method stub
+		return mMapper.enrolledCount(member_id);
+	}
+	@Override
+	public List<CourseEnrollmentVO> lastAccessedCourse(int member_id) {
+		// TODO Auto-generated method stub
+		return mMapper.lastAccessedCourse(member_id);
+	}
+	@Override
+	public MyMemberVO memberProfileData(int member_id) {
+		// TODO Auto-generated method stub
+		return mMapper.memberProfileData(member_id);
+	}
+
+}
