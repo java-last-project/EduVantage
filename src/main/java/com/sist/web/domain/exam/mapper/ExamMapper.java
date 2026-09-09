78637776e83c75d3758a18b14eb0130f9274e72f
ki-sd <redringo4@gmail.com>
2026-09-10 08:56:44 +0900

    [Feature/exam] kisd) 시험 응시 및 제출, 채점 로직 구현 (#47)
    * kisd) feat: 시험 목록 구현 및 메인페이지 배너 추가
    
    * kisd) feat: 시험 목록 구현 및 메인페이지 배너 추가
    
    * kisd) feat: 메인페이지 상시시험 목록 수정
    fix: 시험등록 쿼리 수정
    
    * kisd) feat: 시험 응시 페이지 타이머 및 프로그레스바 구현
    
    * kisd) feat: 시험 응시 및 제출 로직 구현, 채점 로직 구현

--- src/main/java/com/sist/web/domain/exam/mapper/ExamMapper.java
+++ src/main/java/com/sist/web/domain/exam/mapper/ExamMapper.java
@@ -2,6 +2,7 @@ package com.sist.web.domain.exam.mapper;
 
 import java.util.*;
 
+import com.sist.web.domain.exam.vo.ExamUserAnswerVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
@@ -15,4 +16,17 @@ public interface ExamMapper {
 	
 	public List<ExamQuestionVO> examDetailData(Map<String,Object> map);
 	public void insertEnrollment(ExamEnrollmentVO vo);
+	public String getScheduledExamTitle(Integer examNo);
+	public ExamEnrollmentVO findActiveEnrollment(Map<String,Object> map);
+	public List<ExamQuestionVO> getQuestionForGrading(List<Integer> qno);
+	public void insertUserAnswers(List<ExamUserAnswerVO> answers);
+	public void updateEnrollmentFinish(ExamEnrollmentVO vo);
+	public List<Map<String, Object>> selectPendingSubjectiveList(int graderId);
+	public int claimGradingTask(Map<String, Object> map);
+	public void releaseGradingClaim(Map<String, Object> map);
+	public void gradeSubjectiveAnswer(Map<String, Object> map);
+	public int countRemainingPending(int enrollmentNo);
+	public void finalizeEnrollmentScore(int enrollmentNo);
+	public Map<String, Object> selectExamResultMaster(int enrollmentNo);
+	public List<Map<String, Object>> selectExamResultDetails(int enrollmentNo);
 }
