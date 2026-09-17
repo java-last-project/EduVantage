d140887051195f19d6af5e3698e2c24448a80f44
ki-sd <redringo4@gmail.com>
2026-09-17 17:26:59 +0900

    kisd) feat: 시험 사이드바의 "응시장" 버튼 분기처리

--- src/main/java/com/sist/web/domain/exam/service/ExamService.java
+++ src/main/java/com/sist/web/domain/exam/service/ExamService.java
@@ -12,6 +12,7 @@ public interface ExamService {
 	public String getExamTitle(Integer examNo);
 	public int getExamLimitMinutes(Integer examNo);
 	public Map<String, Object> submitExam(int memberId,Map<String, Object> params);
+	public ExamEnrollmentVO getResumableEnrollment(int memberId, Integer enrollmentNo);
 	public Map<String, Object> getExamResultData(int memberId,int enrollmentNo);
 	public Integer getScheduledExamResult(int memberId,int examNo);
 	public List<Map<String, Object>> getMyExamList(int memberId);
